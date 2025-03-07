1d336392b6a90a68c58d500016540bd7b607b60b
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-03-07 20:24:51 +0530

    fixing foreign key issue

--- vege/models.py
+++ vege/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Recipe(models.Model):
-  # user = models.ForeignKey(User, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) #foreign key throws an error if null and blank are not specified since django then confuses it with the id.
   recipe_name=models.CharField(max_length=100)
   recipe_desc=models.TextField()
   recipe_image=models.ImageField(upload_to="vege/images")
