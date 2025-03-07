d740ef44ca006e0a228b32b9ceade313e9821c1e
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-03-08 01:24:41 +0530

    winding up recipes and registering on admin panel

--- vege/admin.py
+++ vege/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Recipe)       #important if you want to view any created model in admin panel
